0a50fe745ec50fe2e1e779da559cad79433cb47c
Kristian Lubina <kristian.lubina@outlook.com>
2026-09-12 14:42:16 +0200

    Update general-config.js

--- study-visualization/program-specific/standard-config/general-config.js
+++ study-visualization/program-specific/standard-config/general-config.js
@@ -8,5 +8,7 @@ window.StudiengangGeneralConfig = {
   // Optional HTML subtitle (renders clickable links when provided)
   subtitleHtml: `<a href="https://www.fhnw.ch/de/studium/technik/maschinenbau" target="_blank" rel="noopener">Studiengang-Seite</a>
     <br>
-    <a href="https://issuu.com/fhnw/docs/modultabelle_20maschinenbau" target="_blank" rel="noopener">Alle Module Maschinenbau</a>`,
+    <a href="https://issuu.com/fhnw/docs/modultabelle_20maschinenbau" target="_blank" rel="noopener">Alle Module Maschinenbau</a>
+    <br>
+    <a href="https://www.fhnw.ch/++api++/de/technik-umwelt/studium/angebot/studiengaenge/bachelorstudium-maschinenbau/moduluebersicht-bachelor-maschinenbau-ht-fhnw.pdf/@@inline-file/file" target="_blank" rel="noopener">Curriculum</a>`,
 };
